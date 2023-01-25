51fa3f8dc04aa8ec5440cc94db8f3f0466e090b1
Ink <inkofdeath230@gmail.com>
2023-01-25 02:26:46 -0500

    final bit review in bitbasics

--- BinaryBasics/Program.cs
+++ BinaryBasics/Program.cs
@@ -15,8 +15,29 @@ public class Program
     Console.WriteLine($"{numberTwo} is represented as:");
     Print(numberTwo);
 
+    Console.WriteLine();
 
+    Console.WriteLine($"{numberOne} & {numberTwo} is:");
+    Print(numberOne & numberTwo);
+    Console.WriteLine();
 
+    Console.WriteLine($"{numberOne} | {numberTwo} is:");
+    Print(numberOne | numberTwo);
+    Console.WriteLine();
 
+    Console.WriteLine($"{numberOne} ^ {numberTwo} is:");
+    Print(numberOne ^ numberTwo);
+    Console.WriteLine();
+
+    Console.WriteLine($"~{numberOne} is:");
+    Print(~numberOne);
+    Console.WriteLine();
+
+    Console.WriteLine($"~{numberTwo} is:");
+    Print(~numberTwo);
+    Console.WriteLine();
+
+    Console.WriteLine($"If we add 1 to {numberOne} we get:");
+    Print(numberOne + 1);
   }
 }
