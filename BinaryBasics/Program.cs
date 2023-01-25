7724af246682eba7a76d5f08f4dc563498e5f58b
Ink <inkofdeath230@gmail.com>
2023-01-25 02:22:18 -0500

    initial binary display

--- BinaryBasics/Program.cs
+++ BinaryBasics/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using static BitHelper.BitHelper;
+public class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.WriteLine("Enter two numbers to compare them in binary.");
+    int numberOne = Convert.ToInt32(Console.ReadLine());
+    int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"{numberOne} is represented as:");
+    Print(numberOne);
+
+    Console.WriteLine();
+
+    Console.WriteLine($"{numberTwo} is represented as:");
+    Print(numberTwo);
+
+
+
+
+  }
+}
