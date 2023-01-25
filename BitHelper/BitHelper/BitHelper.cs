1daf62a1bff21dc8cfdb16d1613e1725d9d03493
Ink <inkofdeath230@gmail.com>
2023-01-25 02:06:55 -0500

    BitHelper to print binary strings

---
+++ BitHelper/BitHelper/BitHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BitHelper;
+
+public class BitHelper
+{
+  public static void Print(int binary, int digits = 8, int spacing = 4)
+  {
+    string result = Convert.ToString(binary, 2).PadLeft(digits, '0');
+
+    result = Regex.Replace(result, ".{" + spacing + "}", "$0 ");
+
+    Console.WriteLine($"B: {result}");
+  }
+}
