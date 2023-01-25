cd636ed94a6ca1f7a3b48681b28a378e24fa6145
Ink <inkofdeath230@gmail.com>
2023-01-25 03:13:18 -0500

    Menu system and menu selector; try catch safety

---
+++ BinaryAlgorithms/MenuChoices.cs
@@ -0,0 +1,10 @@
+﻿namespace BinaryAlgorithms;
+
+public enum MenuChoices
+{
+  CountOnes = 1,
+  CheckPowerOfTwo = 2,
+  CheckBitSet = 3,
+  OutputAllSubsets = 4,
+  LargestPowerOfTwo = 5
+}
--- BinaryAlgorithms/Program.cs
+++ BinaryAlgorithms/Program.cs
@@ -1,9 +1,110 @@
-﻿namespace BinaryAlgorithms;
+﻿using static BitHelper.BitHelper;
+
+namespace BinaryAlgorithms;
 
 public class Program
 {
   static void Main(string[] args)
   {
+    bool loop = true;
+    int choice, number;
 
+    while (loop)
+    {
+      try
+      {
+        choice = Menu();
+        if (choice == 0) loop = false;
+        Console.WriteLine();
+
+        Console.Write("Enter a number to use: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        MenuSelector(choice, number);
+
+        Console.WriteLine();
+        Console.WriteLine("Hit any key to continue.");
+        Console.ReadLine();
+        Console.Clear();
+      }
+      catch
+      {
+        Console.WriteLine();
+        Console.WriteLine("Invalid numeric integer input");
+        Console.WriteLine("Hit any key to continue.");
+        Console.ReadLine();
+        Console.Clear();
+      }
+    }
+  }
+
+  public static int Menu()
+  {
+    Console.WriteLine($@"Please make a choice.
+
+1. Count the amount of one's in a number's binary representation.
+2. Check if a number is a power of 2.
+3. Check if a particular bit (2^ith) is set in a number.
+4. Output all subsets of a given set.
+5. Find the largest power within a number.
+0. Exit.
+");
+    Console.Write("Enter your choice: ");
+    return Convert.ToInt32(Console.ReadLine());
+  }
+
+  public static void MenuSelector(int choice, int number)
+  {
+    Console.Clear();
+    Console.WriteLine($"Using the number {number} with choice {choice}");
+    Console.Write($"{number} as binary is ");
+    Print(number);
+    Console.WriteLine();
+
+    switch (choice)
+    {
+      case (int)MenuChoices.CountOnes:
+        CountOnes(number);
+        break;
+
+      case (int)MenuChoices.CheckPowerOfTwo:
+        //Call a static Program function PowerOfTwo()
+        break;
+
+      case (int)MenuChoices.CheckBitSet:
+        //Call a static Program function CheckIfBitSet()
+        break;
+
+      case (int)MenuChoices.OutputAllSubsets:
+        //Call a static Program function OutputAllSubsets()
+        break;
+
+      case (int)MenuChoices.LargestPowerOfTwo:
+        //Call a static Program function LargestPowerOfTwo()
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  public static void CountOnes(int number)
+  {
+    Console.WriteLine("Counting the number of one's in {4}...");
+    Console.WriteLine();
+
+    int count = 0;
+    int initial = number;
+    while (number != 0)
+    {
+      Console.WriteLine($"Count: {count} || n: {number} || {Convert.ToString(number, 2)}");
+      number &= number - 1;
+      count++;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"The number of one's in {initial} is: {count}");
+    Print(initial);
   }
 }
